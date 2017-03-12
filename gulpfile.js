bdb1db2df40488db15dfaf7304020ae0d1cd509f
Rob Thuleen <rob.thuleen@gmail.com>
2017-03-12 09:13:31 -0700

    added promise to module task

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,26 @@ var fs = require('fs');
 var path = require('path');
 
 function npmInstall(module) {
-    var cmd = exec('cd client &&  npm install @common/' + module + ' -f && cd ../server && npm install @common/' + module + ' -f')
-    cmd.stdout.on('data', function (data) {
-        process.stdout.write(data);
-    });
-    cmd.stderr.on('data', function (data) {
-        process.stderr.write(data);
-    });
+    return new Promise(function (resolve, reject) {
+        var cmd = exec('cd client &&  npm install @common/' + module + ' -f && cd ../server && npm install @common/' + module + ' -f', function (err) {
+            if (err) return reject(err);
+            return resolve();
+        })
+        cmd.stdout.on('data', function (data) {
+            process.stdout.write(data);
+        });
+        cmd.stderr.on('data', function (data) {
+            process.stderr.write(data);
+        });
+    })
+
 }
 
 gulp.task('modules', function () {
     return gulp.src(['common/*'], function (err, files) {
         files.forEach(function (file) {
             if (fs.lstatSync(file).isDirectory()) {
-                npmInstall(path.basename(file));
+                return npmInstall(path.basename(file));
             }
         })
     })
@@ -26,6 +32,6 @@ gulp.task('modules', function () {
 
 gulp.task('watch', ['modules'], function () {
     return gulp.watch(['common/**/*.js', '!common/**/node_modules/**'], function (event) {
-        npmInstall(path.basename(path.dirname(event.path)));
+        return npmInstall(path.basename(path.dirname(event.path)));
     });
 })
